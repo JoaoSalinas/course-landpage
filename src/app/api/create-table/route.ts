c98cbac3b233650406f3f2886a1dd55f9263bf29
JoaoSalinas <joao.salinas@outlook.com>
2024-03-06 19:26:10 -0300

    Ajusta query de create da tabela

--- src/app/api/create-table/route.ts
+++ src/app/api/create-table/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   try{
-    const result = await sql`CREATE TABLE PreEnrollment (Name varchar(255), Email varhar(255) Enrollment varchar(255), PhoneNumber(25))`    
+    const result = await sql`CREATE TABLE PreEnrollment (Name varchar(255), Email varchar(255), Enrollment varchar(255), PhoneNumber varchar(25))`    
     return NextResponse.json({ result }, {status: 200})
   } catch (error) {
     return NextResponse.json({ error }, {status: 500})
