8fea93ec9b8038cc4942da8c812f5911d2cd3325
JoaoSalinas <joao.salinas@outlook.com>
2024-03-11 16:04:08 -0300

    Ajusta resposta do faq

--- src/app/_components/faq-section.tsx
+++ src/app/_components/faq-section.tsx
@@ -9,18 +9,18 @@ import {
 function FaqSection() {
   return (
     <section id="faq" className='bg-slate-50 dark:bg-transparent container space-y-6 py-8 md:py-12 lg:py-24'>
-     <HeadingSections title="FAQ"/>
-     <div className='mx-auto text-center md:max-w-[58rem]'>
+      <HeadingSections title="FAQ" />
+      <div className='mx-auto text-center md:max-w-[58rem]'>
         <Accordion type='single' collapsible className='w-full'>
           <AccordionItem value='item-1'>
             <AccordionTrigger>Como comprar o curso?</AccordionTrigger>
             <AccordionContent key="item-1" className='text-left'>
               <p>
-              Caso esteja no período de inscrições abertas, simplesmente clique no botão <span className="font-bold">
-                Matricular</span>. Você será encaminhado diretamente para a página de finalização da compra. Após a conclusão, o acesso às aulas será enviado imediatamente para o seu e-mail.
+                Caso esteja no período de inscrições abertas, simplesmente clique no botão <span className="font-bold">
+                  Matricular</span>. Você será encaminhado diretamente para a página de finalização da compra. Após a conclusão, o acesso às aulas será enviado imediatamente para o seu e-mail.
               </p>
               <p>
-              Se o período de inscrições já tiver terminado, você tem a opção de clicar no botão {' '}
+                Se o período de inscrições já tiver terminado, você tem a opção de clicar no botão {' '}
                 <span className='font-bold'>Lista de Espera </span>. Ao preencher o formulário, você será notificado assim que as matrículas estiverem disponíveis novamente.
               </p>
             </AccordionContent>
@@ -29,12 +29,37 @@ function FaqSection() {
             <AccordionTrigger>Para quem é este curso?</AccordionTrigger>
             <AccordionContent key="item-2" className='text-left'>
               <p>
-                Para quem possui conhecimentos básicos de HTML, CSS e JavaScript
+                Este curso é meticulosamente projetado para atender uma ampla gama de participantes interessados em aprofundar seus conhecimentos e habilidades no universo da programação, desde o nível iniciante até o intermediário, com o objetivo de alcançar uma competência sólida em desenvolvimento de software.
               </p>
-              <p>Para quem quer aprender desenvolver uma aplicação do zero</p>
-              <p>
-                Para quem quer estar atualizado com os conhecimentos que as
-                grandes empresas procuram
+              <p className="my-2"> A seguir, <span className='font-bold'>detalhei os perfis para quem este curso é particularmente adequado</span>:</p>
+              <ol className="list-decimal list-inside space-y-4">
+                <li>
+                  <span className='font-bold'>Iniciantes em Programação</span>: Se você está dando seus primeiros passos na programação e deseja construir uma fundação robusta em lógica de programação, princípios de programação orientada a objetos (POO), padrões de design de software, e muito mais, este curso oferece um caminho estruturado desde o zero.
+                </li>
+
+                <li>
+                  <span className='font-bold'>Desenvolvedores Aspirantes</span>: Para aqueles que têm alguma experiência com programação, mas desejam formalizar e expandir seus conhecimentos para se tornarem desenvolvedores profissionais, o curso abrange tópicos avançados e práticas da indústria, como engenharia de software, metodologias ágeis (incluindo Scrum), e introdução a inteligências artificiais aplicadas ao desenvolvimento.
+                </li>
+                <li>
+
+                  <span className='font-bold'>Profissionais de TI</span>: Profissionais já atuantes em outras áreas da tecnologia da informação que buscam transição para o desenvolvimento de software ou que desejam aprimorar suas competências em programação, análise e desenvolvimento de sistemas encontrarão no curso uma oportunidade de evolução e diversificação de suas habilidades.
+                </li>
+
+                <li>
+                  <span className='font-bold'>Educadores e Mentores em Tecnologia</span>: Instrutores e mentores que desejam atualizar seus conhecimentos ou buscar inspiração para novas metodologias de ensino em programação, engenharia de software, e gestão de projetos de tecnologia.
+                </li>
+
+                <li>
+                  <span className='font-bold'>Empreendedores Digitais</span>: Aqueles que estão planejando lançar startups tecnológicas ou desejam entender melhor o desenvolvimento de software para gerenciar equipes de tecnologia de forma mais eficaz.
+                </li>
+
+                <li>
+                  <span className='font-bold'>Hobbistas e Autodidatas</span>: Indivíduos apaixonados por tecnologia que buscam uma estrutura de aprendizado formal para complementar seus estudos autodidatas em programação e desenvolvimento de sistemas.
+                </li>
+              </ol>
+
+
+              <p className="mt-2">Além disso, o curso é ideal para quem valoriza a aplicação prática de conhecimentos, dada a sua ênfase em projetos reais e estudos de caso, preparando os participantes não apenas para passar em entrevistas de emprego na área de tecnologia, mas também para enfrentar desafios do mundo real no desenvolvimento de software. Portanto, se você se identifica com algum dos perfis mencionados ou simplesmente tem um interesse genuíno em tecnologia e inovação, este curso foi feito pensando em você.
               </p>
             </AccordionContent>
           </AccordionItem>
@@ -43,7 +68,7 @@ function FaqSection() {
               Por quanto tempo terei acesso ao curso?
             </AccordionTrigger>
             <AccordionContent key="item-3" className='text-left'>
-              Você terá acesso vitalício a este curso e todas suas atualizações. 
+              Você terá acesso vitalício a este curso e todas suas atualizações.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value='item-4'>
@@ -87,7 +112,7 @@ function FaqSection() {
           </AccordionItem>
         </Accordion>
       </div>
-    </section>
+    </section >
   )
 }
 
