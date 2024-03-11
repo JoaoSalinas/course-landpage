2731d3ded9db1a8d6f77dcb972074a5515db20fe
JoaoSalinas <joao.salinas@outlook.com>
2024-03-11 07:03:35 -0300

    Altera posição do toggle do tema e conteudo de texto

--- src/app/_components/intro-section.tsx
+++ src/app/_components/intro-section.tsx
@@ -3,8 +3,11 @@ import { ModeToggle } from "./mode-toggle"
 import { buttonVariants } from "@/components/ui/button"
 
 export function IntroSection(){
-  return (
+  return (    
     <section id="intro" className="space-y-6 py-8 md:py-12 lg:py-32 relative">
+    <div className="flex flex-1 justify-end">
+      <ModeToggle/>
+    </div>
     <svg viewBox="0 0 1024 1024" className="absolute left-1/3 top-full md:left-1/2 md:top-1/2 -z-10 h-[16rem] w-[16rem] md:h-[54rem] md:w-[54rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
       <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
       <defs>
@@ -16,7 +19,6 @@ export function IntroSection(){
     </svg>
     <div className="container flex max-w-[64rem] flex-col items-center text-center gap-4">
       <span className="rounded-2xl bg-muted px-4 py-1.5 text-smfont-medium flex items-center mb-8">🗓️ 1ª turma dia 05 de Março de 2024
-        <ModeToggle/>
       </span>
       <h1 className="font-heading text-4xl md:text-6xl ls:text-7xl">Inicie agora a sua carreira
         <br />
@@ -29,9 +31,9 @@ export function IntroSection(){
           Mesmo sem nunca ter escrito uma única linha de código.
         </span>
       </p>
-      <div className="space-x-4">
-        <a href="#investiment" className={cn(buttonVariants({ variant: 'outline', size: 'lg', className: "bg-primary py-2 px-4 text-md text-white rounded shadow-lg font-bold py-8" }))}>Impulsionar a carreira</a>
-        <a href="#feature" className={cn(buttonVariants({ variant: 'outline', size: 'lg', className: 'py-8' }))}>O que vamos aprender?</a>
+      <div className="space-x-4 flex flex-col md:flex-row gap-y-4">
+        <a href="#investiment" className={cn(buttonVariants({ variant: 'outline', size: 'lg', className: "bg-primary border-primary px-4 text-md text-white rounded shadow-lg shadow-primary font-bold py-8" }))}>Impulsionar a minha carreira</a>
+        <a href="#feature" className={cn(buttonVariants({ variant: 'outline', size: 'lg', className: 'py-8' }))}>Saiba mais sobre o curso</a>
         <button></button>
       </div>
     </div>
